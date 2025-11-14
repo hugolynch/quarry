29d411ee446e255456ad774b1b460a593f8ed9cf
Hugo Lynch <hugo@hopscotch.ca>
2025-11-14 08:38:54 -0500

    New icon for submit button

--- src/components/WordArea.svelte
+++ src/components/WordArea.svelte
@@ -201,7 +201,8 @@
     {/if}
     <button on:click={submitWord} disabled={game.currentWord.length === 0 || game.gameOver}>
       <svg class="button-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        <polyline points="20,6 9,17 4,12"></polyline>
+        <polyline points="9 10 4 15 9 20"></polyline>
+        <path d="M20 4v7a4 4 0 0 1-4 4H4"></path>
       </svg>
       Submit
     </button>
